5451350517fe81b860a82e82994521be1bdd958d
4Source <38220764+4Source@users.noreply.github.com>
2024-02-11 20:25:13 +0100

    Add trycatch save/loadProfileData

--- src/util/SettingsFiles.ts
+++ src/util/SettingsFiles.ts
@@ -9,25 +9,28 @@ import { ensurePathExist, getAllFiles, isValidPath } from "./FileSystem";
  * @param profilesPath The path where the profiles should be saved 
  */
 export function saveProfileData(profilesList: ProfileSetting[], profilesPath: string) {
-    profilesList.forEach(profile => {
-        // Ensure is valid profile
-        if (!profile) {
-            throw Error("Can't save undefined profile!");
-        }
-        // Ensure is valid path
-        if (!isValidPath([profilesPath, profile.name])) {
-            throw Error("Invalid path received!")
-        }
-        // Ensure path exist
-        if (!ensurePathExist([profilesPath, profile.name])) {
-            throw Error("The path cannot be accessed!")
-        }
+    try {
+        profilesList.forEach(profile => {
+            // Ensure is valid profile
+            if (!profile) {
+                throw Error("Can't save undefined profile!");
+            }
+            // Ensure is valid path
+            if (!isValidPath([profilesPath, profile.name])) {
+                throw Error("Invalid path received!")
+            }
+            // Ensure path exist
+            ensurePathExist([profilesPath, profile.name]);
 
-        // Write profile settings to path
-        const file = join(profilesPath, profile.name, "profile.json");
-        const profileSettings = JSON.stringify(profile, null, 2);
-        writeFileSync(file, profileSettings, 'utf-8');
-    });
+            // Write profile settings to path
+            const file = join(profilesPath, profile.name, "profile.json");
+            const profileSettings = JSON.stringify(profile, null, 2);
+            writeFileSync(file, profileSettings, 'utf-8');
+        });
+    } catch (e) {
+        (e as Error).message = 'Failed to save profile data! ' + (e as Error).message;
+        throw e;
+    }
 }
 
 /**
@@ -35,18 +38,23 @@ export function saveProfileData(profilesList: ProfileSetting[], profilesPath: st
  * @param profilesPath The path where the profiles are saved
  */
 export function loadProfileData(profilesPath: string) {
-    // Search for all profiles existing
-    const files = getAllFiles([profilesPath, "/*/profile.json"]);
-    let profilesList: ProfileSetting[] = [];
+    try {
+        // Search for all profiles existing
+        const files = getAllFiles([profilesPath, "/*/profile.json"]);
+        let profilesList: ProfileSetting[] = [];
 
-    // Read profile settings
-    files.forEach(file => {
-        if (existsSync(file) && statSync(file).isFile()) {
-            const data = readFileSync(file, "utf-8");
-            profilesList.push(JSON.parse(data));
-        }
-    });
-    return profilesList;
+        // Read profile settings
+        files.forEach(file => {
+            if (existsSync(file) && statSync(file).isFile()) {
+                const data = readFileSync(file, "utf-8");
+                profilesList.push(JSON.parse(data));
+            }
+        });
+        return profilesList;
+    } catch (e) {
+        (e as Error).message = 'Failed to load profile data! ' + (e as Error).message;
+        throw e;
+    }
 }
 
 /**
